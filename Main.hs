73fab92ba5d3adadef6c7b598d60cc780122fe27
ulisses <ulissesmonhecosta@gmail.com>
2009-01-20 02:06:50 +0000

    done 1st milestone

--- Main.hs
+++ Main.hs
@@ -2,36 +2,32 @@
 module Main where
 
 import Network.Pcap
+import Net.Packet
+import qualified Net.PacketParsing as NPP
 import Net.Ethernet
 import Net.IPv4
 import Net.ICMP
 import Data.ByteString as B
 import Data.ByteString.Internal
+import Foreign.ForeignPtr
+import Foreign.Ptr
+import Data.Word
+-- import Network.Pcap.Base
+import Foreign.Marshal.Array (peekArray)
 import IO
 import Char
+import Maybe
 
 f = openOffline "snort.log"
 
 callback1 :: PktHdr -> ByteString -> IO ()
-callback1 pkt bs = B.putStrLn bs
+callback1 pkt bs = print $  ppp $ unpack bs
+--callback1 pkt bs = print $ packType $ fromJust $ ppp $ unpack bs
 
 ff = f >>= \x -> loopBS x (-1) callback1
 
-magia = openFile "snort.log" ReadMode  >>=  System.IO.Encoding.hGetContents ASCII >>= return . Prelude.take 1000
-magia2 =  B.readFile "snort.log"   >>=  return . decode ASCII
+p bytes = toInPack (listArray (0,Prelude.length bytes-1) bytes)
 
--- import Foreign.ForeignPtr
--- import Foreign.Ptr
--- import Data.Word
--- import Network.Pcap.Base
--- import Foreign.Marshal.Array (peekArray)
--- 
--- main = do
---         p <- openOffline "snort.log"
---         s <- withForeignPtr p $ \ptr -> do loop ptr (-1) printIt
---         return ()
--- 
--- printIt :: PktHdr -> Ptr Word8 -> IO ()
--- printIt ph bytep = do
---     a <- peekArray (fromIntegral (hdrCaptureLength ph)) bytep
---     print a
+pp bytes = NPP.doParse $ p bytes :: Maybe (Net.Ethernet.Packet (Net.IPv4.Packet Net.ICMP.Packet))
+
+ppp bytes = NPP.doParse $ p bytes:: Maybe (Net.Ethernet.Packet InPacket)
